85ac0c0ad0f0ed56bf9ebce32ca4793671699a3f
beyourahi <rahikhan360@gmail.com>
2022-06-09 13:49:46 +0600

    project setup

---
+++ components/layout.tsx
@@ -0,0 +1,12 @@
+import Head from 'next/head'
+
+export const Layout: React.FC<{ children: React.ReactNode }> = ({ children }) => (
+  <main>
+    <Head>
+      <title>Raihana Rezvi</title>
+      <meta name="description" content="Personal website of Raihana Rezvi" />
+      {/* <link rel="icon" href="/favicon.ico" /> */}
+    </Head>
+    {children}
+  </main>
+)
